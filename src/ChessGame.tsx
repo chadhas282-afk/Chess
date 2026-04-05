a7690aeceb500888cd0e8c9a61fbec1b5a884b8f
chadhas282-afk <chadhas282@gmail.com>
2026-04-05 11:21:57 +0530

    Add piece retrieval logic in getRawMoves function

--- src/ChessGame.tsx
+++ src/ChessGame.tsx
@@ -41,5 +41,6 @@ const ChessGame: React.FC = () => {
   }}
 
   function getRawMoves(pos: Position, currentBoard: (Piece | null)[][]): Position[] {
+    const piece = currentBoard[pos.row][pos.col];
     
   }
