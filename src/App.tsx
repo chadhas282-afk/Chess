c62896048510ff94b3d193675bdd9e0560916dbc
chadhas282-afk <chadhas282@gmail.com>
2026-04-14 21:33:27 +0530

    add App function definition

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ChessGame from './ChessGame';
 import './App.css';
 
-// function App() {
+function App() {
 //   return (
 //     <div className="App">
 //       <ChessGame />
 //     </div>
 //   );
-// }
+}
 
 // export default App;
