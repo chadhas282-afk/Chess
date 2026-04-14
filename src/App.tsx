d672d62e43de0a07425f3718e21f7fb99a19cf88
chadhas282-afk <chadhas282@gmail.com>
2026-04-14 21:32:59 +0530

    add import statements in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// import React from 'react';
-// import ChessGame from './ChessGame';
-// import './App.css';
+import React from 'react';
+import ChessGame from './ChessGame';
+import './App.css';
 
 // function App() {
 //   return (
